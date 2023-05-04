3372b1eb14375dee9f921f6651a13f56ee8b4025
YiLong <2488174858@qq.com>
2023-05-04 15:14:57 +0800

    配置文件补充

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
+  // lintOnSave: false, // 关闭语法检查 防止不必要的语法报错
+  // // 配置devServer
+  // devServer: {
+  //   host: 'localhost', // 项目运行的ip
+  //   port: 8888 // 项目运行的端口号
+  // }
 })
